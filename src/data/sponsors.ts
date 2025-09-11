3d8268c29f63d15dbcbc3cc1fb8544077e0135a8
Ayushmaan Shandilya <90828689+ayushmaanxs@users.noreply.github.com>
2025-09-11 15:09:14 +0530

    New sponsor added

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -3,4 +3,5 @@ export const sponsors: string[] = [
   "https://res.cloudinary.com/dswkss8nj/image/upload/v1757440961/38830c24-e451-4dc3-a7e7-008ede49f3ce_aekoem.jpg",
   "https://res.cloudinary.com/dswkss8nj/image/upload/v1757440887/WhatsApp_Image_2025-09-09_at_23.21.55_e3f3ad67_bfihx8.jpg",
   "https://res.cloudinary.com/dswkss8nj/image/upload/v1757560643/TVS_kzzbhn.jpg",
+  "https://res.cloudinary.com/dswkss8nj/image/upload/v1757583453/WhatsApp_Image_2025-09-11_at_15.05.25_ad32d93b_hnvsfp.jpg",
 ];
