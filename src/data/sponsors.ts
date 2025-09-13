1c8f9c8df2ca796614c2b24c2e9c518c05613f66
Ayushmaan Shandilya <90828689+ayushmaanxs@users.noreply.github.com>
2025-09-13 11:04:28 +0530

    Sposnsors fixed

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -1,5 +1,5 @@
 export const sponsors: string[] = [
-  "https://res.cloudinary.com/dswkss8nj/image/upload/v1757560643/TVS_kzzbhn.jpg",
+  "https://res.cloudinary.com/dswkss8nj/image/upload/v1757741292/tvs_logo.jpg",
   "https://res.cloudinary.com/dswkss8nj/image/upload/v1757698907/WhatsApp_Image_2025-09-12_at_23.04.36_29a90ea5_h6je96.jpg",
   "https://res.cloudinary.com/dp8njkkpz/image/upload/v1757340129/sponsor1_fzbgo1.jpg",
   "https://res.cloudinary.com/dswkss8nj/image/upload/v1757440961/38830c24-e451-4dc3-a7e7-008ede49f3ce_aekoem.jpg",
